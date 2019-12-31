842758b8880b3cb9dabf498a071875336d48dfe1
Justin Chin <justinmichaelchin@gmail.com>
2019-12-30 20:05:07 -0800

    checkpoint commit

--- onlisp.lisp
+++ onlisp.lisp
@@ -1,4 +1,4 @@
-;; Common lisp has distinct namespaces for variables and functions
+; Common lisp has distinct namespaces for variables and functions
 ;; e.g it is a LISP-2
 
 ;; If we have overlapping namespaces, we can use the following
@@ -372,3 +372,341 @@
         (if val
             (values (car lst) val)
             (find2 fn (cdr lst))))))
+
+;; some utility functions for operating on lists
+(proclaim '(inline last1 single append1 conc1 mklist))
+
+(defun last1 (lst)
+  (car (last lst)))
+
+(defun single (lst)
+  (and (consp lst) (not (cdr lst))))
+
+(defun append1 (lst obj)
+  (append lst (list obj)))
+
+(defun conc1 (lst obj)
+  (nconc lst (list obj)))
+
+(defun mklist (obj)
+  (if (listp obj) obj (list obj)))
+
+(defun longer (x y)
+  (labels ((compare (x y)
+             (and (consp x)
+                  (or (null y)
+                      (compare (cdr x) (cdr y))))))
+    (if (and (listp x) (listp y))
+        (compare x y)
+        (> (length x) (length y)))))
+
+(defun filter (fn lst)
+  (let ((acc nil))
+    (dolist (x lst)
+      (let ((val (funcall fn x)))
+        (if val (push val acc))))
+    (nreverse acc)))
+
+(defun group (source n)
+  (when (zerop n) (error "zero length"))
+  (labels ((rec (source acc)
+             (let ((rest (nthcdr n source)))
+               (if (consp rest)
+                   (rec rest (cons (subseq source 0 n) acc))
+                   (nreverse (cons source acc))))))
+    (if source (rec source nil) nil)))
+
+(defun flatten (lst)
+  (labels ((rec (lst acc)
+             (cond ((null lst) acc)
+                   ((atom lst) (cons lst acc))
+                   (t
+                    (rec (car lst) (rec (cdr lst) acc))))))
+    (rec lst nil)))
+
+(defun prune (pred tree)
+  (labels ((rec (tree acc)
+             (cond ((null tree) (nreverse acc))
+                   ((consp (car tree))
+                    (rec (cdr tree)
+                         (cons (rec (car tree) nil) acc)))
+                   (t
+                    (rec (cdr tree)
+                         (if (funcall pred (car tree))
+                             acc
+                             (cons (car tree) acc)))))))
+    (rec tree nil)))
+
+(defun find2 (fn lst)
+  (if (null lst)
+      nil
+      (let ((val (funcall fn (car lst))))
+        (if val
+            (values (car lst) val)
+            (find2 fn (cdr lst))))))
+
+(defun before (x y lst &key (test #'eql))
+  "Recursively CDR down LST, applying TEST to each CAR. If Y passes
+the test first, return nil, else return the remainder of the
+untraversed list."
+  (and lst
+       (let ((first (car lst)))
+         (cond ((funcall test y first) nil)
+               ((funcall test x first) lst)
+               (t
+                (before x y (cdr lst) :test test))))))
+
+(defun after (x y lst &key (test #'eql))
+  (let ((rest (before y x lst :test test)))
+    (and rest (member x rest :test test))))
+
+(defun duplicate (obj lst &key (test #'eql))
+  "Returns a list at the point OBJ is first duplicated, else NIL"
+  (member obj
+          (cdr (member obj lst :test test)) :test test))
+
+(defun split-if (pred lst)
+  "Returns 2 lists split at the point where PRED is True"
+  (let ((acc nil))
+    (do ((src lst (cdr src)))
+        ((or (null src) (funcall pred (car src)))
+         (values (nreverse acc) src))
+      (push (car src) acc))))
+
+(defun most (fn lst)
+  (if (null lst)
+      (values nil nil)
+      (let* ((wins (car lst))
+             (max (funcall fn wins)))
+        (dolist (obj (cdr lst))
+          (let ((score (funcall fn obj)))
+            (when (> score max)
+              (setf wins obj
+                    max score))))
+        (values wins max))))
+
+(defun best (fn lst)
+  (if (null lst)
+      nil
+      (let ((wins (car lst)))
+        (dolist (obj (cdr lst))
+          (if (funcall fn obj wins)
+              (setf wins obj)))
+        wins)))
+
+(defun mostn (fn lst)
+  (if (null lst)
+      (values nil nil)
+      (let ((result (list (car lst)))
+            (max (funcall fn (car lst))))
+        (dolist (obj (cdr lst))
+          (let ((score (funcall fn obj)))
+            (cond ((> score max)
+                   (setf max score
+                         result (list obj)))
+                  ((= score max)
+                   (push obj result)))))
+        (values (nreverse result) max))))
+
+;;;; MAPPING
+;;;; ----------------------------------------------------------------------
+
+(defun map0-n (fn n)
+  (mapa-b fn 0 n))
+
+(defun map1-n (fn n)
+  (mapa-b fn 1 n))
+
+(defun mapa-b (fn a b &optional (step 1))
+  (do ((i a (+ i step))
+       (result nil))
+      ((> i b) (nreverse result))
+    (push (funcall fn i) result)))
+
+(defun map-> (fn start test-fn succ-fn)
+  (do ((i start (funcall succ-fn i))
+       (result nil))
+      ((funcall test-fn i) (nreverse result))
+    (push (funcall fn i) result)))
+
+(defun mappend (fn &rest lsts)
+  (apply #'append (apply #'mapcar fn lsts)))
+
+(defun mapcars (fn &rest lsts)
+  (let ((result nil))
+    (dolist (lst lsts)
+      (dolist (obj lst)
+        (push (funcall fn obj) result)))
+    (nreverse result)))
+
+(defun rmapcar (fn &rest args)
+  (if (some #'atom args)
+      (apply fn args)
+      (apply #'mapcar
+             #'(lambda (&rest args)
+                 (apply #'rmapcar fn args))
+             args)))
+
+;; (rmapcar #'princ '(1 2 (3 4 (5) 6) 7 (8 9)))
+;; (rmapcar #'+ '(1 (2 (3) 4)) '(10 (20 (30) 40)))
+
+;;;; I/O
+;;;; ----------------------------------------------------------------------
+
+(defun readlist (&rest args)
+  (values (read-from-string
+           (concatenate 'string "(" (apply #'read-line args) ")"))))
+
+(defun prompt (&rest args)
+  (apply #'format *query-io* args)
+  (read *query-io*))
+
+(defun break-loop (fn quit &rest args)
+  (format *query-io* "Entering break-loop. ~%")
+  (loop
+    (let ((in (apply #'prompt args)))
+      (if (funcall quit in)
+          (return)
+          (format *query-io* "~A~%" (funcall fn in))))))
+
+;;;; Strings and Symbols
+;;;; ----------------------------------------------------------------------
+
+(defun mkstr (&rest args)
+  (with-output-to-string (s)
+    (dolist (a args) (princ a s))))
+
+(defun symb (&rest args)
+  (values (intern (apply #'mkstr args))))
+
+(defun reread (&rest args)
+  (values (read-from-string (apply #'mkstr args))))
+
+(defun explode (sym)
+  (map 'list #'(lambda (c)
+                 (intern (make-string 1
+                                      :initial-element c)))
+       (symbol-name sym)))
+
+;;;; Returning Functions
+;;;; ----------------------------------------------------------------------
+
+;; use `complement' when you need to negate a predicate
+
+(defvar *!equivs* (make-hash-table))
+
+;; functions and their destructive counterparts are usually known
+;; before runtime, so for maximum efficiency define `!' as a macro or
+;; provide a read macro for it
+(defun ! (fn)
+  "Returns the destructuve equivalent of fn if defined in *!equivs*"
+  (or (gethash fn *!equivs*) fn))
+
+(defun def! (fn fn!)
+  "Maps a function to its destructive equivalent"
+  (setf (gethash fn *!equivs*) fn!))
+
+;;;; Memoizing
+;;;; ----------------------------------------------------------------------
+
+(defun memoize (fn)
+  "Returns a closure which caches the results of applying fn"
+  (let ((cache (make-hash-table :test #'equal)))
+    #'(lambda (&rest args)
+        (multiple-value-bind (val win) (gethash args cache)
+          (if win
+              val
+              (setf (gethash args cache)
+                    (apply fn args)))))))
+
+(defun compose (&rest fns)
+  ""
+  (if fns
+      (let ((fn1 (car (last fns)))
+            (fns (butlast fns)))
+        #'(lambda (&rest args)
+            (reduce #'funcall fns
+                    :from-end t
+                    :initial-value (apply fn1 args))))
+      #'identity))
+
+(defun fif (if then &optional else)
+  #'(lambda (x)
+      (if (funcall if x)
+          (funcall then x)
+          (if else (funcall else x)))))
+
+(defun fint (fn &rest fns)
+  (if (null fns)
+      fn
+      (let ((chain (apply #'fint fns)))
+        #'(lambda (x)
+            (and (funcall fn x) (funcall chain x))))))
+
+(defun fun (fn &rest fns)
+  (if (null fns)
+      fn
+      (let ((chain (apply #'fun fns)))
+        #'(lambda (x)
+            (or (funcall fn x) (funcall chain x))))))
+
+;; because `not' is a lisp function, we can define complement in terms
+;; of it, using composition
+
+;; (defun complement (pred) (compose #'not p)
+
+(defun our-length (lst)
+  (if (null lst)
+      0
+      (1+ (our-length (cdr lst)))))
+
+(defun our-every (fn lst)
+  (if (null lst)
+      t
+      (and (funcall fn (car lst))
+           (our-every fn (cdr lst)))))
+
+;; consider the two functions defined above, and the similiarities in
+;; their structure. Both functions recur down the cdr of their list
+;; arguments, and return a base value when the list is exhausted. This
+;; pattern comes up so often that any experiened programmer can
+;; instantly see the shared structure and will be wondering if we can
+;; capture this abstraction cleanly
+
+(defun lrec (rec &optional base)
+  "Constructs a list recursing function. REC is a function of two
+arguments, the car of the list being recurred upon, and the function
+embodying the recursive call."
+  (labels ((self (lst)
+             (if (null lst)
+                 (if (functionp base)
+                     (funcall base)
+                     base)
+                 (funcall rec (car lst) #'(lambda ()
+                                            (self (cdr lst)))))))
+    #'self))
+
+;; copy-list
+(lrec #'(lambda (x f) (cons x (funcall f))))
+
+;; remove-duplicates
+(lrec #'(lambda (x f) (adjoin x (funcall f))))
+
+;; find-if, for some function fn
+(lrec #'(lambda (x f) (if (fn x) x (funcall f))))
+
+;; some, for some function fn
+(lrec #'(lambda (x f) (or (fn x) (funcall f))))
+
+;;; Note: While lrec may be rather readable for an experienced lisper,
+;;; it will usually produce inoptimal code because it is not accouting
+;;; for a tail-recursive solution
+
+;;;; Recursion on Subtrees
+;;;; ----------------------------------------------------------------------
+
+(defun rfind-if (fn tree)
+  (if (atom tree)
+      (and (funcall fn tree) tree)
+      (or (rfind-if fn (car tree))
+          (if (cdr tree) (rfind-if fn (cdr tree))))))
